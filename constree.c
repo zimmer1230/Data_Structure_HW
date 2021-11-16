c4b7ed86072040f0e0b4a5185a1bf73817d2f994
zimmer1230 <pao.wen25@gmail.com>
2021-11-16 19:09:35 +0800

    after demo

--- constree.c
+++ constree.c
@@ -51,13 +51,15 @@ int main(){
     bfs(0);
     //bfs for distance
 
-
+    // get the list of the node by distance
     for(int i=0;i<how_many_nodes;i++){
         temp[i].ID=nodes[i].ID;
         temp[i].dist=nodes[i].dist_from_0;
     }
     qsort(temp,how_many_nodes,sizeof(struct comparing_for_dist),cmp);
+    // get the list of the node by distance
 
+    //transfer weight and counting cost
     for(int i=0;i<how_many_nodes-1;i++){
         if( nodes[ temp[i].ID ].weight%packet_size==0 )
             total_cost+= (nodes[ temp[i].ID ].weight/packet_size);
@@ -65,15 +67,16 @@ int main(){
             total_cost+= (nodes[ temp[i].ID ].weight/packet_size)+1;
         nodes[ nodes[ temp[i].ID ].parent_node ].weight += nodes[ temp[i].ID ].weight;
     }
+    //transfer weight and counting cost
 
-//    for(int i=0; i<how_many_nodes;i++){
-//        printf("ID=%d weight=%d parent=%d\n",nodes[i].ID,nodes[i].weight,nodes[i].parent_node);
-//    }
 
+    //output
     printf("%d %d\n0 0\n",how_many_nodes,total_cost);
     for(int i=1;i<how_many_nodes;i++){
         printf("%d %d\n",i,nodes[i].parent_node );
     }
+    //output
+
 
 return 0;
 }
@@ -82,6 +85,8 @@ int QQ[MAX_NODES];
 int QQ_index=0;
 void bfs(int p){
     nodes[p].explored=1;
+
+    //add 0's adjacent nodes to QQ
     for( int i=0;i<nodes[p].num_edge; i++ ){
         if( nodes[ nodes[p].links_to[i] ].explored==0 ){
             nodes[ nodes[p].links_to[i] ].dist_from_0 = nodes[p].dist_from_0 + 1;
@@ -90,6 +95,9 @@ void bfs(int p){
             QQ[QQ_index++]=nodes[p].links_to[i];
         }
     }
+    //add 0's adjacent nodes to QQ
+
+    //doing bfs
     for( int i=0; i<QQ_index; i++ ){
         for( int j=0; j<nodes[ QQ[i] ].num_edge; j++ ){
             if( nodes[ nodes[ QQ[i] ].links_to[j] ].explored == 0 ){
@@ -100,6 +108,8 @@ void bfs(int p){
             }
         }
     }
+    //doing bfs
+
 }
 void prt_QQ(){
     for(int i=0;i<QQ_index;i++){
