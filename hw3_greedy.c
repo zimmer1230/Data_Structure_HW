62660d6dfa441dfa3b3079d6f6d1a8e17cc83260
zimmer1230 <pao.wen25@gmail.com>
2021-12-09 16:49:52 +0800

    func

--- hw3_greedy.c
+++ hw3_greedy.c
@@ -10,13 +10,24 @@ struct node{
     double y;
     int transmit_num;
     struct node* transmit_to[MAX_NODES];
+    int is_transmitter;
 };
 typedef struct node node;
 
-node nodes[MAX_NODES];
+struct link{
+    node* transmitter;
+    node* receiver;
+};
+typedef struct link link;
 
+node nodes[MAX_NODES];
+link links[MAX_NODES*5];
+
+int is_valid_to_append();
+void append();
 
 int main(){
+    /* Get input */
     scanf("%d%d%d%d",&how_many_nodes,&how_many_links,&power,&noise);
     for(int i=0;i<how_many_nodes;i++){
         int node_id;
@@ -28,7 +39,25 @@ int main(){
         scanf("%d",&link_id);
         scanf("%d%d",&node_1,&node_2);
         nodes[node_1].transmit_to[nodes[node_1].transmit_num++ ] = &nodes[node_2];
+        links[link_id].transmitter=&nodes[node_1];
+        links[link_id].receiver=&nodes[node_2];
+        nodes[node_1].is_transmitter=1;
     }
 
+    for(int i=0;i<how_many_links;i++){
+        if( is_valid_to_append() ){
+            append();
+        }
+
+    }
+
+
     return 0;
+}
+
+double current_
+
+int is_valid_to_append(){
+
+
 }
