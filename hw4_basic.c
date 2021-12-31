a6d170e9fc7d947a80568637b66a93130f942828
zimmer1230 <61425969+zimmer1230@users.noreply.github.com>
2022-01-01 02:19:40 +0800

    bit

--- hw4_basic.c
+++ hw4_basic.c
@@ -22,8 +22,81 @@ int main(){
     while(n--){
         scanf("%d",&input);
         printf("%d ",input);
-
-
+#define h_input input%p%m
+        switch(h_input%8){
+        case 0:
+            if(bucket[h_input/8].f_0==0){
+                printf("1\n");
+                bucket[h_input/8].f_0=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 1:
+            if(bucket[h_input/8].f_1==0){
+                printf("1\n");
+                bucket[h_input/8].f_1=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 2:
+            if(bucket[h_input/8].f_2==0){
+                printf("1\n");
+                bucket[h_input/8].f_2=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 3:
+            if(bucket[h_input/8].f_3==0){
+                printf("1\n");
+                bucket[h_input/8].f_3=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 4:
+            if(bucket[h_input/8].f_4==0){
+                printf("1\n");
+                bucket[h_input/8].f_4=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 5:
+            if(bucket[h_input/8].f_5==0){
+                printf("1\n");
+                bucket[h_input/8].f_5=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 6:
+            if(bucket[h_input/8].f_6==0){
+                printf("1\n");
+                bucket[h_input/8].f_6=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        case 7:
+            if(bucket[h_input/8].f_7==0){
+                printf("1\n");
+                bucket[h_input/8].f_7=1;
+            }
+            else{
+                printf("0\n");
+            }
+            break;
+        }
 
     }
 
